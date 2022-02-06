f2d61c166f675d65c32c9c74329c52cce0ca837f
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-07 01:18:38 +0530

    [Guru] Added FindYourNum.cs practice program

---
+++ DataStructure_Algorithms_PracticeProblems/Program.cs
@@ -0,0 +1,20 @@
+﻿using DataStructure_Algorithms_PracticeProblems.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Algorithms_PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DataStructure and Algorithms Practice Problems");
+
+            FindYourNum.FindYourNumber();
+            Console.ReadLine();
+        }
+    }
+}
